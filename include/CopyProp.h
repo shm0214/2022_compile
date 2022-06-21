d75320a6ae8f29d3ecbfffeb1606b1bde9654e04
yanshihui <2398220094@qq.com>
2022-06-21 10:20:24 +0800

    mrevise

--- include/CopyProp.h
+++ include/CopyProp.h
@@ -2,11 +2,13 @@
 #define __COPY_PROP_H
 
 #include <vector>
-#include<tr1/unordered_map>//在unordered_map之前加上tr1库名，
+// #include<tr1/unordered_map>//在unordered_map之前加上tr1库名，
+#include<unordered_map>
 #include "Unit.h"
 #define USS unordered_map<SymbolEntry*, SymbolEntry*>
 #define USO unordered_map<SymbolEntry*, Operand*>
-using namespace std::tr1;
+// using namespace std::tr1;
+using namespace std;
 class CopyProp {
     Unit* unit;
    public:

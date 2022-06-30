1db9cb325f52e0bfabb85dfcec0fe6e8858fbea0
yanshihui <2398220094@qq.com>
2022-06-30 19:24:11 +0800

    test1

--- src/Ast.cpp
+++ src/Ast.cpp
@@ -720,6 +720,9 @@ int ExprNode::fold_const(bool &flag){
         }
         else return 0;
     }
+    else if(this->getSymbolEntry()->isConstant()){
+        return ((ConstantSymbolEntry*)(this->getSymbolEntry()))->getValue();
+    }
     flag = 0;
     return 0;
 }

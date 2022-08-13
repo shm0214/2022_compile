fe94f511982f4fc00e4ec3a45a0bf6c5a15ac7cb
shm0214 <1186562408@qq.com>
2022-08-13 10:53:49 +0800

    float support for optimize

--- src/main.cpp
+++ src/main.cpp
@@ -148,7 +148,7 @@ int main(int argc, char* argv[]) {
         po.pass();
         mdce.pass();
         ms.pass();
-        is.pass();
+        // is.pass();
     }
     if (dump_asm)
         mUnit.output();

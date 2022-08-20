65c4fb7f6da0735e0f68f067bb13f48f220b9ef1
shm0214 <1186562408@qq.com>
2022-08-20 21:08:14 +0800

    test

--- src/main.cpp
+++ src/main.cpp
@@ -145,19 +145,19 @@ int main(int argc, char* argv[]) {
         PeepholeOptimization po(&mUnit);
         PartialRedundancyElimination pre(&mUnit);
         LocalValueNumber lvn(&mUnit);
-        caaz.pass();
-        ca.pass();
-        // 效果一般 而且会导致编译时间长一些 不开了
-        // pre.pass();
-        mdce.pass();
-        ms.pass();
-        po.pass1();
-        mdce.pass();
-        lvn.pass();
-        mdce.pass();
-        po.pass();
-        mdce.pass();
-        ms.pass();
+        // caaz.pass();
+        // ca.pass();
+        // // 效果一般 而且会导致编译时间长一些 不开了
+        // // pre.pass();
+        // mdce.pass();
+        // ms.pass();
+        // po.pass1();
+        // mdce.pass();
+        // lvn.pass();
+        // mdce.pass();
+        // po.pass();
+        // mdce.pass();
+        // ms.pass();
     }
 
     if (!optimize) {
@@ -172,9 +172,9 @@ int main(int argc, char* argv[]) {
         MachineStraighten ms(&mUnit);
         PeepholeOptimization po(&mUnit);
         InstructionScheduling is(&mUnit);
-        po.pass();
-        mdce.pass();
-        ms.pass();
+        // po.pass();
+        // mdce.pass();
+        // ms.pass();
         // is.pass();
     }
     if (dump_asm)
